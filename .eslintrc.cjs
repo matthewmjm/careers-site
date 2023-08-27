313b609ea5448b69d49e09c36dda518f797c9b2b
matthewmjm <matthewmjm@yahoo.com>
2023-08-27 10:10:39 -0600

    vitest config and Main Nav component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,11 +7,15 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
+    "plugin:vitest-globals/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
   },
+  env: {
+    "vitest-globals/env": true,
+  },
   plugins: [
     'tailwindcss'
-  ]
-};
+  ],
+}
--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1 class="mt-8 text-center text-6xl">Hello Vue</h1>
+  <!-- <h1 class="mt-8 text-center text-6xl">Hello Vue</h1> -->
+  <MainNav />
 </template>
+
+<script setup>
+import MainNav from "@/components/MainNav.vue";
+</script>
